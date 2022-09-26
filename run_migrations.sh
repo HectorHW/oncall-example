5f89fa54bd1534af7fcd7621993a897bd8cf43c6
Vladimir Redkin <vsredkin01@gmail.com>
2022-09-26 22:09:23 +0300

    retry logic for migrations

--- run_migrations.sh
+++ run_migrations.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #this script is used to apply migrations to database (see docker-compose.yml)
-mysql -u root -p1234 -h mysql < ./db/schema.v0.sql
+for i in 1 2 3 4 5; do mysql -u root -p1234 -h mysql < ./db/schema.v0.sql && break || sleep 5; done
